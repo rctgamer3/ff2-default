bc50af71ebf51ed950318916a2a11adb8a4bea66
rctgamer3 <rctgamer3@hotmail.com>
2012-03-05 20:28:30 +0100

    Added install button and button install script

---
+++ js/install.js
@@ -0,0 +1,13 @@
+function install (aEvent)
+{
+  var params = {
+    "Foo": { URL: aEvent.target.href,
+             IconURL: aEvent.target.getAttribute("iconURL"),
+             Hash: aEvent.target.getAttribute("hash"),
+             toString: function () { return this.URL; }
+    }
+  };
+  InstallTrigger.install(params);
+
+  return false;
+}
